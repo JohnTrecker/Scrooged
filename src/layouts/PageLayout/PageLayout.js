44735564b08491bf2a940f26f6af8c776edeee54
johntrecker <jttrecker@gmail.com>
2017-06-09 15:14:01 -0700

    adds chart route

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -9,6 +9,8 @@ export const PageLayout = ({ children }) => (
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    {' · '}
+    <Link to='/chart' activeClassName='page-layout__nav-item--active'>Chart</Link>
     <div className='page-layout__viewport'>
       {children}
     </div>
